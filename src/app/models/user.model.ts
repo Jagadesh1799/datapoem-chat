2b3a95d8678097c52e7eea8e3dcf4a9befffa19b
Jagadesh1799 <jagadesh1799@gmail.com>
2023-04-18 19:15:07 +0530

    Auto-generated commit [ci skip]

--- src/app/models/user.model.ts
+++
@@ -1,9 +0,0 @@
-export interface IUser {
-  image: IImage;
-  username: string;
-}
-
-export interface IImage {
-  png: string;
-  webp: string;
-}
--- src/app/pages/index.ts
+++
@@ -1,2 +0,0 @@
-export  * from './comment/comment.component';
-export  * from './delete-confirmation-dialog/delete-confirmation-dialog.component';
